112338d37279de219f067de3e622a671660a415f
yosiaLukumai <yosialukumai@gmail.com>
2022-12-22 16:15:24 +0300

    added the dash access cards

--- src/pages/dash/home.vue
+++ src/pages/dash/home.vue
@@ -1,15 +1,75 @@
 <template>
-  <div class="q-pa-md text-center text-h6">
-    Amazing dashboard coming
+  <div class="q-pa-md">
+    <div class="row q-gutter-sm">
+      <div class="col-md-3 col-12">
+        <q-card class="my-card q-pa-sm" bordered>
+          <q-card-section>
+            <q-item dense>
+              <q-item-section>
+                <q-icon name="bed" size="2rem" color="secondary"></q-icon>
+              </q-item-section>
+              <q-item-section class="text-bold text-primary text-h6">
+                Venues</q-item-section
+              >
+            </q-item>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-md-3 col-12">
+        <div class="col-md-3 col-12">
+          <q-card class="my-card q-pa-sm" bordered>
+            <q-card-section>
+              <q-item dense>
+                <q-item-section>
+                  <q-icon name="bed" size="2rem" color="secondary"></q-icon>
+                </q-item-section>
+                <q-item-section class="text-bold text-primary text-h6">
+                  Venues</q-item-section
+                >
+              </q-item>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <div class="col-md-3 col-12">
+        <div class="col-md-3 col-12">
+          <q-card class="my-card q-pa-sm" bordered>
+            <q-card-section>
+              <q-item dense>
+                <q-item-section>
+                  <q-icon name="bed" size="2rem" color="secondary"></q-icon>
+                </q-item-section>
+                <q-item-section class="text-bold text-primary text-h6">
+                  Venues</q-item-section
+                >
+              </q-item>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-12">
+        <div class="col-md-3 col-12">
+          <q-card class="my-card q-pa-sm" bordered>
+            <q-card-section>
+              <q-item dense>
+                <q-item-section>
+                  <q-icon name="bed" size="2rem" color="secondary"></q-icon>
+                </q-item-section>
+                <q-item-section class="text-bold text-primary text-h6">
+                  Venues</q-item-section
+                >
+              </q-item>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
